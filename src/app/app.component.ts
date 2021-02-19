d17e1ce79acb7ba031dcfa39bc69fdacdd3edd0a
Emmanuel <d.emmanuel.duque@gmail.com>
2021-02-19 20:10:13 +0100

    code2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,81 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'table';
+  datos: any;
+  public rows: Array<any> = [];
+  public columns: Array<any> = [];
+  public page: number = 1;
+  public itemsPerPage: number = 10;
+  public maxSize: number = 5;
+  public numPages: number = 1;
+  public length: number = 0;
+
+  public config: any = {
+    paging: true,
+    sorting: { columns: this.columns },
+    filtering: { filterString: '' },
+    className: ['table-striped', 'table-bordered']
+  };
+  dataArray: any[];
+
+  settings = {
+    columns: {
+      id: {
+        title: 'ID'
+      },
+      title: {
+        title: 'Title'
+      },
+      url: {
+        url: 'URl'
+      }
+    }
+  };
+
+
+  constructor(private http: HttpClient) {
+    this.GetDatos()
+  }
+
+
+
+  GetDatos() {
+    let url = "https://jsonplaceholder.typicode.com/photos";
+    this.http.get(url).subscribe(res => {
+      this.dataArray = [];
+      this.dataArray.push(res);
+
+      this.dataArray.map(res => {
+        console.log('RESSS', res);
+        this.datos = res;
+      })
+      return res;
+    })
+  }
+
+
+  descargarExcel(){
+    this.exportAsExcelFile(this.datos, 'prueba')
+  }
+
+
+  public exportAsExcelFile(json: any[], excelFileName: string): void {
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    console.log('sss', ws);
+
+    // const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    // XLSX.utils.book_append_sheet(wb, ws, 'TESTTT');
+
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
+  }
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http'
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    Ng2SmartTableModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
